8c496f4c0f8b103bb0529728be94352301e57317
Agistra Arifur Rahman <agistraarifurrahman007@gmail.com>
2024-01-30 15:56:41 +0700

    delete migration history

--- prisma/migrations/20231221061655_authsetup/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `Customer` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `customer` ADD COLUMN `type` ENUM('ANALOG', 'INET', 'DIGITAL') NOT NULL;
